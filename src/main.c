92c38071e8755565b9881dace34cd32295aeb406
Eddy0402 <yukkcake@gmail.com>
2015-01-30 00:36:25 +0800

    Add some macro to calculate joystick output
    temporary use gyro plotter to view result, the angle of joystick mapped linearly
    to roll, pitch setpoint.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,16 @@
 #include "stm32f4xx_hal.h"
 #include "uart.h"
 #include "joystick.h"
+#include "send.h"
+#include "arm_math.h"
 
+#define X_MID_VALUE 1291
+#define Y_MID_VALUE 2037
+#define ADC_MAX_VALUE 4095
+#define ADC_MIN_VALUE 0
+#define ROLL_MAX 20
+#define PITCH_MAX 20
+#define CENTER_AREA 0.03
 
 int main(void){
     HAL_Init();
@@ -14,6 +23,7 @@ int main(void){
 
     while(1){
         uint16_t X = 0, Y = 0;
+        float x = 1.0, y = 10;
         ADC_SwitchChannel(0);
         HAL_ADC_PollForConversion(&AdcHandle, 10000);
         if(HAL_ADC_GetState(&AdcHandle) == HAL_ADC_STATE_EOC_REG)
@@ -28,6 +38,20 @@ int main(void){
             Y = HAL_ADC_GetValue(&AdcHandle);
         }
 
-        kprintf("%d %d\r\n", X, Y);
+        x = (X > X_MID_VALUE) ?
+            (float)(X - X_MID_VALUE) / (ADC_MAX_VALUE - X_MID_VALUE) :
+            (float)(X - X_MID_VALUE) / (X_MID_VALUE - ADC_MIN_VALUE);
+
+        y = (Y > Y_MID_VALUE) ?
+            (float)(Y - Y_MID_VALUE) / (ADC_MAX_VALUE - Y_MID_VALUE) :
+            (float)(Y - Y_MID_VALUE) / (Y_MID_VALUE - ADC_MIN_VALUE);
+
+        x = ((x > 0 ? x : -x) < CENTER_AREA) ? 0 : x;
+        y = ((y > 0 ? y : -y) < CENTER_AREA) ? 0 : y;
+
+        // Temporary use gyro plot canvas to see the output
+        float g[3] = { 0, x * ROLL_MAX, y * PITCH_MAX};
+
+        SendCommand_3( 0x01 , 0x00, (uint8_t *)g, 12);
     }
 }
